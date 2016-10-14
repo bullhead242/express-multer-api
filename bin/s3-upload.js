c071c33afcd14bb29dee30141d078d0ddda743dd
bullhead242 <bullhead242@gmail.com>
2016-10-14 11:46:09 -0400

    Comments added throughout file   s3 upload method defined     crypto module used to name files     nameFile and nameDirectory functions used to determine upload path

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,22 +1,30 @@
 'use strict';
 
+// this has to come before anything else
+require('dotenv').config();
+
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
+const AWS = require('aws-sdk');
 
 const filename = process.argv[2] || '';
 
 const readFile = (filename) => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.readFile(filename, (error, data) => {
       if (error) {
         reject(error);
       }
+
       resolve(data);
     });
   });
 };
 
-// set default object if typeData is an unsupported filetype
+// return a default object in the case that fileType is given an unsupported
+// filetype to read
 const mimeType = (data) => {
   return Object.assign({
     ext: 'bin',
@@ -30,31 +38,72 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
-const upload = (file) => {
-  const options = {
-    // get bucket name from AWS s3 console
-    Bucket:'ps.bucket.10.2016',
-    // attch fileBuffer as a stream to send to AWS
-    Body: file.data,
-    // give public access to read URL of uploaded file
-    ACL: 'public-read',
-    // send mime type of uploaded file
-    ContentType: file.mime,
-    // pick filename for AWS to use for uploaded file
-    Key: `test/test.${file.ext}`
-  };
-  // continue to pass data along Promise chain
-  return Promise.resolve(options);
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
 };
 
-const logMessage = (upload) => {
-  // temporarily delete stream to log options in terminal without stream
-  delete upload.Body;
-  console.log(`upload options are ${JSON.stringify(upload)}`);
+const nameFile = (file) => {
+  return randomHexString(16)
+  .then((val) => {
+    file.name = val;
+    return file;
+  });
+};
+
+const nameDirectory = (file) => {
+  file.dir = new Date().toISOString().split('T')[0];
+  return file;
+};
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  }
+});
+
+const upload = (file) => {
+  const options = {
+    // get the bucket name from your AWS S3 console
+    Bucket: 'ps.bucket.10.2016',
+    // attach the fileBuffer as a stream to send to S3
+    Body: file.data,
+    // allow anyone to access the URL of the uploaded file
+    ACL: 'public-read',
+    // tell S3 what the mime-type is
+    ContentType: file.mime,
+    // pick a filename for S3 to use for the upload
+    Key: `${file.dir}/${file.name}.${file.ext}`
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.upload(options, (error, data) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(data);
+    });
+  });
+};
+
+const logMessage = (response) => {
+  // turn the pojo into a string so I can see it on the console
+  console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
 .then(upload)
 .then(logMessage)
 .catch(console.error)
