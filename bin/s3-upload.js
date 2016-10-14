cdae24c9443fe8e28a7f3a57bd1b4b3effd35c76
bullhead242 <bullhead242@gmail.com>
2016-10-14 14:01:06 -0400

    Save information of upload to datastore
    Create and use 'upload' model and mongoose w closed database connection

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 
 const upload = require('../lib/s3-upload').upload;
 
+const mongoose = require('../app/middleware/mongoose');
+const Upload = require('../app/models/upload');
+
 const filename = process.argv[2] || '';
+const comment = process.argv[3] || 'No comment';
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -18,6 +22,14 @@ const readFile = (filename) => {
   });
 };
 
+const createUpload = (response) => {
+  let upload = {
+    location: response.Location,
+    comment: comment,
+  };
+  return Upload.create(upload);
+};
+
 const logMessage = (response) => {
   // turn the pojo into a string so I can see it on the console
   console.log(`the response from AWS was ${JSON.stringify(response)}`);
@@ -25,6 +37,8 @@ const logMessage = (response) => {
 
 readFile(filename)
 .then(upload)
+.then(createUpload)
 .then(logMessage)
 .catch(console.error)
-;     
+.then(() => mongoose.connection.close())
+;
